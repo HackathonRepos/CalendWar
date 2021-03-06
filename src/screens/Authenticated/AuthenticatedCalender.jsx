981679e2d1c4c70ca242da083477430d81b9562f
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-06 14:38:17 -0800

    Added api and called requests when user picked on calender

--- src/screens/Authenticated/AuthenticatedCalender.jsx
+++ src/screens/Authenticated/AuthenticatedCalender.jsx
@@ -1,13 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import Select from "@material-ui/core/Select";
 import axios from "axios";
 
 function AuthenticatedCalendar() {
   const [value, onChange] = useState(new Date());
+  const [historicalData, setHistoricalData] = useState({});
   const month = value.getMonth() + 1;
-  console.log(month);
+  const day = value.getDate();
+  const handleChange = (val) => {
+    onChange(val);
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://history.muffinlabs.com/date/${month}/${day}`
+      )
+      .then(({ data }) => {
+        console.log(data);
+        setHistoricalData(data);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/http://history.muffinlabs.com/date/${month}/${day}`
+      )
+      .then(({ data }) => {
+        console.log(data);
+        setHistoricalData(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <Navbar />
@@ -15,7 +40,7 @@ function AuthenticatedCalendar() {
         <div style={{ marginLeft: "20px" }}>
           <h1>Pick a date!</h1>
           <Calendar
-            onChange={onChange}
+            onChange={handleChange}
             value={value}
             className="react-calendar"
             defaultView="year"
@@ -28,7 +53,7 @@ function AuthenticatedCalendar() {
             display: "flex",
           }}
         >
-          <h1>Events and Conflicts</h1>
+          <h1>Events and Conflicts on {historicalData["date"]}</h1>
         </div>
       </div>
     </div>

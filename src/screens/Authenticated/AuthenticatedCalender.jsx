3798d1ce2337906fc6f372653d6a65e0a0b2ba07
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-06 15:40:47 -0800

    Added option to select between events, deaths, births

--- src/screens/Authenticated/AuthenticatedCalender.jsx
+++ src/screens/Authenticated/AuthenticatedCalender.jsx
@@ -4,11 +4,14 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import axios from "axios";
 import Event from "../../components/CalenderComponents/Event";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import { mockData } from "./original";
 
 function AuthenticatedCalendar() {
   const [value, onChange] = useState(new Date());
   const [historicalData, setHistoricalData] = useState(mockData);
+  const [historyType, setHistoryType] = useState("Events");
   const month = value.getMonth() + 1;
   const day = value.getDate();
   const handleChange = (val) => {
@@ -21,6 +24,9 @@ function AuthenticatedCalendar() {
         setHistoricalData(data);
       })
       .catch((err) => console.log(err));
+  };
+  const changeType = (evt) => {
+    setHistoryType(evt.target.value);
   };
   useEffect(() => {
     axios
@@ -38,7 +44,9 @@ function AuthenticatedCalendar() {
       <Navbar />
       <div className="CalenderContent">
         <div style={{ marginLeft: "20px" }}>
-          <h1>Pick a date!</h1>
+          <div>
+            <h1>Pick a date!</h1>
+          </div>
           <Calendar
             onChange={handleChange}
             value={value}
@@ -54,9 +62,21 @@ function AuthenticatedCalendar() {
             flexDirection: "column",
           }}
         >
-          <h1>Events and Conflicts on {historicalData["date"]}</h1>
           <div>
-            {historicalData["data"]["Events"].map((ev, i) => (
+            <h1>Events and Conflicts on {historicalData["date"]}</h1>
+            <Select
+              labelId="demo-simple-select-filled-label"
+              id="demo-simple-select-filled"
+              value={historyType}
+              onChange={changeType}
+            >
+              <MenuItem value={"Events"}>Events</MenuItem>
+              <MenuItem value={"Births"}>Births</MenuItem>
+              <MenuItem value={"Deaths"}>Deaths</MenuItem>
+            </Select>
+          </div>
+          <div>
+            {historicalData["data"][historyType].map((ev, i) => (
               <Event year={ev.year} description={ev.text} key={i} />
             ))}
           </div>

5d88189c30062721f58784e35adb07a7c2f4d2d9
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-03-06 02:47:43 -0800

    Added firebase initialization

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,34 @@
+import dotenv from "dotenv";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
 
+import firebase from "firebase";
+dotenv.config();
+
+function InitFirebase() {
+  if (!firebase.apps.length) {
+    firebase.initializeApp({
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_API_KEY_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_API_KEY_MESSAGE_ID,
+      appId: process.env.REACT_APP_FIREBASE_API_KEY_APP_ID,
+    });
+  } else {
+    firebase.app();
+  }
+  console.log("Initialized Firebase");
+  return <App />;
+}
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <InitFirebase />
   </BrowserRouter>,
   document.getElementById("root")
 );
